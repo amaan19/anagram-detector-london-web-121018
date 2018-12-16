f24e3975cae3399074c77dd5edc9872545dae05b
amaan19 <amaan.ahmed3@gmail.com>
2018-12-16 11:03:22 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+# Your code goes here
+require 'pry'
+class Anagram
+
+  attr_accessor :word
+
+  def initialize (word)
+    @word = word
+  end
+
+  def match(anagram_array)
+      arranged_letters = self.word.split("").sort
+
+    anagram_array.select {|anagram|
+      arranged_array_letters = anagram.split("").sort
+        arranged_letters == arranged_array_letters
+    }
+  end
+end
